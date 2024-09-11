dce0571c044a1483581be77a94f4c6a05b9b0d52
lavralex <lavralex2015@yandex.ru>
2024-09-11 23:58:44 +0300

    добавление карточек при загрузке и функция удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const content = document.querySelector(".content");
+const cardsContainer = content.querySelector(".places__list");
 
-// @todo: DOM узлы
+function cardCreate(cardContent, cardDelete) {
+  const card = cardTemplate.querySelector(".places__item").cloneNode(true);
+  card.querySelector(".card__image").setAttribute("src", cardContent.link);
+  card.querySelector(".card__title").textContent = cardContent.name;
+  cardDelete(card);
+  cardsContainer.append(card);
+}
 
-// @todo: Функция создания карточки
+function cardDelete(card) {
+  deleteButton = card.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", function () {
+    card.remove();
+  });
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+for (i = 0; i < initialCards.length; i += 1) {
+  let cardContent = initialCards[i];
+  cardCreate(cardContent, cardDelete);
+}
